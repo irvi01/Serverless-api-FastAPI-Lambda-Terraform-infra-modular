1695291fc3838d4f9fc09fb9e24d21b98d42858b
Yrwing <yrwing.new@gmail.com>
2025-08-14 18:40:11 -0300

    ci: add github actions deploy pipeline with OIDC

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,5 +1,11 @@
 # Configuração do backend para o Terraform
 # Aqui usamos o S3 como backend para armazenar o estado do Terraform
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "desafio-tfstate-irvi-1755189166"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "desafio-tf-locks"
+    encrypt        = true
+  }
 }
