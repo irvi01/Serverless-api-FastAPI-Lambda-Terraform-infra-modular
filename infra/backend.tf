4d15d135b0d1571af6a52452592495477834ed69
Yrwing <yrwing.new@gmail.com>
2025-08-14 20:52:51 -0300

    Fixing terraform Lambda module

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@
 # Aqui usamos o S3 como backend para armazenar o estado do Terraform
 terraform {
   backend "s3" {
-    bucket         = "desafio-tfstate-irvi-1755189166"
+    bucket         = "challenge-entrevista"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "desafio-tf-locks"
